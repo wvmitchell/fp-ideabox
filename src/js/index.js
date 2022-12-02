4b75a1aeeef76e3fb829d6c2f4fa30a5e8afb13b
Will Mitchell <wvmitchell@gmail.com>
2022-12-02 15:17:29 -0700

    wire things up to allow for the adding of ideas to the page

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,30 @@
 import startApp from './app.js'
+import { draw, erase, makeIdeaElements } from './draw.js'
 
+// Business Logic
 const app = startApp()
+
+// DOM Elements
+const titleInput = document.getElementById('title')
+const bodyInput = document.getElementById('body')
+const saveButton = document.getElementById('save')
+const searchField = document.getElementById('search')
+const ideaDisplay = document.getElementById('idea-display')
+
+// Drawing Functions
+const drawIdeas = draw(ideaDisplay)
+
+// Event Actions
+const addIdea = event => {
+  const title = titleInput.value
+  const body = bodyInput.value
+
+  app.addIdea({title, body})
+  const ideas = app.getIdeas()
+
+  erase(ideaDisplay)
+  drawIdeas(...makeIdeaElements(ideas))
+}
+
+// Event Listeners
+saveButton.addEventListener('click', addIdea)
